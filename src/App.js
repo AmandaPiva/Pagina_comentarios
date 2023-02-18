5f3ca1322949eedd6679d618e493b124ddea50a1
amanda <amanda.piva01@fatec.sp.gov.br>
2023-02-18 18:02:21 -0300

    Criando propriedades

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     /*Apresentando nosso componente novo no componente principal */
     <div className="App">
-      <Comentario/>  
+      <Comentario nome="João" email="joao@gmail.com" data = {new Date()}>
+        Olá tudo bem?  
+      </Comentario>  
+
+      <Comentario nome="Maria" email="maria@gmail.com" data= {new Date()}>
+        Tudo sim, e com você?  
+      </Comentario>  
     </div>
   );
 }
--- src/Comentarios/Comentarios.js
+++ src/Comentarios/Comentarios.js
@@ -2,9 +2,13 @@ import './Comentarios.css' //importando o css
 import React from "react";  //importando o react 
 
 //Meu primeiro componente
-const Comentario = () => (
+const Comentario = (props) => ( //passando o props como argumento -> obrigatorio
+    /*Passando as propriedades ao DOM */
     <div className="Comp">
-        <div>Olá, como vai?</div>
+        <div>{props.nome}</div>
+        <div>{props.email}</div>
+        <p>{props.children}</p>
+        <p>{props.data.toString()}</p>
     </div>
 );
 
