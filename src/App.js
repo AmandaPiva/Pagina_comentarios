4b4ad36bc43a9e4279868812ef10adb2d8bf1a74
amanda <amanda.piva01@fatec.sp.gov.br>
2023-02-18 16:54:16 -0300

    Criando componente

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Comentario from './Comentarios/Comentarios';
+import './App.css';
+
+function App() {
+  return (
+    /*Apresentando nosso componente novo no componente principal */
+    <div className="App">
+      <Comentario/>  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Comentarios/Comentarios.js
@@ -0,0 +1,11 @@
+import './Comentarios.css' //importando o css
+import React from "react";  //importando o react 
+
+//Meu primeiro componente
+const Comentario = () => (
+    <div className="Comp">
+        <div>Olá, como vai?</div>
+    </div>
+);
+
+export default Comentario; //exportando meu primeiro componente
