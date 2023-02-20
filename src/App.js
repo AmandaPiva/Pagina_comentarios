bc8cc9dfb76c043ff864c78fef591661b189c7b6
amanda <amanda.piva01@fatec.sp.gov.br>
2023-02-20 11:05:30 -0300

    Transformando nossa props em state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import Comentario from './Comentarios/Comentarios';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    /*Apresentando nosso componente novo no componente principal */
-    <div className="App">
-      <Comentario nome="João" email="joao@gmail.com" data = {new Date()}>
-        Olá tudo bem?  
-      </Comentario>  
+class App extends Component{
+  //criando o state
+  //Um state é um objeto com as propriedades
+  state = {
+      //Aqui estamos criando um array no formato de objeto, ele trará os nossos dados dentro de outro objeto
+      comentarios: [
+        {
+          key: "1245",
+          nome: "João",
+          email:"joao@gmail.com",
+          data: new Date(2023, 2, 20),
+          mensagem: "Olá, tudo bem?"
+        },
+        {
+          key: "4562",
+          nome: "Maria",
+          email:"maria@gmail.com",
+          data: new Date(2023, 2, 22),
+          mensagem: "Tudo sim e com você?"
+        }
+      ]
+  }
 
-      <Comentario nome="Maria" email="maria@gmail.com" data= {new Date()}>
-        Tudo sim, e com você?  
-      </Comentario>  
-    </div>
-  );
+  //logo após chamamos o método render() que será responsável por chamar o nosso state e renderizar suas propriedades
+  render(){
+    
+    return( //retorna o nosso state e utilizamos o metodo map para mapearmos o que acontece com cada propriedade do nosso objeto
+      <div className="App">
+        <h1>Meu projeto</h1>
+        
+        {this.state.comentarios.map((comentario, indice) =>(  //colocamos no formato de uma arrow function para realizar a operação do map
+          <Comentario //trasendo consigo nosso componente pronto e com nossos dados 
+            key= {comentario.key}
+            nome= {comentario.nome} 
+            email= {comentario.email} 
+            data = {comentario.data}>
+              {comentario.mensagem}
+          </Comentario>  
+        ))}
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Comentarios/Comentarios.js
+++ src/Comentarios/Comentarios.js
@@ -2,14 +2,14 @@ import './Comentarios.css' //importando o css
 import React from "react";  //importando o react 
 
 //Meu primeiro componente
-const Comentario = (props) => ( //passando o props como argumento -> obrigatorio
-    /*Passando as propriedades ao DOM */
-    <div className="Comp">
+const Comentario = (props) => { //passando o props como argumento -> obrigatorio
+    /*Trazendo as propriedades ao DOM */
+   return <div className="Comp">
         <div>{props.nome}</div>
         <div>{props.email}</div>
         <p>{props.children}</p>
         <p>{props.data.toString()}</p>
     </div>
-);
+};
 
 export default Comentario; //exportando meu primeiro componente
